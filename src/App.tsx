7bfb4aa09982a898229e00269d44d227f97f7587
erez cohen <erezjbh@gmail.com>
2025-08-28 16:30:44 +0300

    add the use state with interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import type { Member } from "./interface/Member.ts";
+import type { Task } from "./interface/Task.ts";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [members, setMembers] = useState<Member[]>([
+    { id: 1, name: "Alice", role: "Dev", isActive: true },
+    { id: 2, name: "Bob", role: "Designer", isActive: false },
+  ]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const [selectedMemberId, setSelectedMemberId] = useState<number | null>(null);
+
+  const [tasks, setTasks] = useState<Task[]>([
+    { id: 1, title: "Create landing page", done: false, assignedTo: 1 },
+    { id: 2, title: "Review mockup", done: true, assignedTo: 2 },
+  ]);
+
+  const [showOnlyActive, setShowOnlyActive] = useState<boolean>(true);
+
+  const [layout, setLayout] = useState<"list" | "grid">("list");
+
+  return <></>;
 }
-
-export default App
